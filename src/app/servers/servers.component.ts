d0c5436c49c0f3aa17fd10e1db517c3685ce0c1e
Carlos(gfcf14) <carloscuba014@gmail.com>
2021-01-29 12:46:47 -0500

    assignment 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created!';
   serverName: string = 'Testserver';
+  username: String = '';
 
   constructor() {
     setTimeout(() => {
@@ -25,6 +26,14 @@ export class ServersComponent implements OnInit {
 
   onUpdateServerName(e: Event) {
     this.serverName = (<HTMLInputElement>e.target).value;
+  }
+
+  isEmpty() {
+    return !this.username.length;
+  }
+
+  resetUsername() {
+    this.username = '';
   }
 
 }
